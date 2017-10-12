11e7106b332a34ac4bec87558986f40bccd2cb75
luffy84217 <luffy84217@gmail.com>
2017-10-12 11:40:53 +0800

    first performance optimization

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { immutable: true, maxAge: 7*24*60*60*1000 }));
 
 app.use('/', index);
 app.use('/api', api);
